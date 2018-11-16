2538001b4ca23adcb09676872d5baf948bcd81fc
Jon Mountjoy <tech@jonmountjoy.com>
2018-11-16 15:24:45 +0000

    Database branch with code that creates table if necessary

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .get("/db", (req, res) => showDatabase(req, res))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+showDatabase = async (req, res) => {
+  try {
+    const client = await pool.connect();
+    await client.query(
+      "create table if not exists test_table (id serial, name text)"
+    );
+    await client.query("insert into test_table (name) values (NOW())");
+    const result = await client.query("SELECT * FROM test_table");
+    const results = { results: result ? result.rows : null };
+    res.render("pages/db", results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+};
